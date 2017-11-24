e10ab0a8c07b4f0ddb7de91a87f9a90395f839bb
Carlos Julio Pérez Quizhpe <carlosjulioperez@gmail.com>
2017-11-24 01:34:47 -0500

    Creando jar ejecutable

--- src/main/java/ec/cjpq/socket/App.java
+++ src/main/java/ec/cjpq/socket/App.java
@@ -1,13 +1,17 @@
 package ec.cjpq.socket;
 
 /**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+ * Clase principal de la aplicación
+ * @author Carlos Julio Pérez Quizhpe carlosjulioperez@gmail.com
+ * 2017-11-23
+ */ 
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+		if (args.length > 0){
+			System.out.println(args[0]);
+		}
+
     }
 }
